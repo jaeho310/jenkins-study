359ff07f1985a86720341fd757bab3ee29952637
jaeho choi <chlwogh1993@gmail.com>
2021-11-17 15:44:51 +0900

    commit

---
+++ src/test/java/com/example/demo/HelloServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloServiceTest {
+
+    @Test
+    void test() {
+        HelloService helloService = new HelloService();
+        assertThat(helloService).isNotNull();
+    }
+
+}
