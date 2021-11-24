02a12b82e6d227c59d527e0da8a12d8e6848504c
jaeho choi <chlwogh1993@gmail.com>
2021-11-24 10:51:39 +0900

    commit

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -11,28 +11,28 @@ node {
         }
         // 이전에 빌드를 한 적이 있고
         // 추가로 가져온 의존성이 없다면 베이스 이미지를 다시 빌드할 필요가 없다.
-        stage('Build Image Check') {
-            def out = new ByteArrayOutputStream(4096)
-            def err = new ByteArrayOutputStream(4096)
-            def proc =  "git diff HEAD HEAD^ go.mod".execute()
-            proc.consumeProcessOutput(out, err)
-            proc.waitFor()
-            def stdOut = out.toString()
-            def errOut = err.toString()
+        // stage('Build Image Check') {
+        //     def out = new ByteArrayOutputStream(4096)
+        //     def err = new ByteArrayOutputStream(4096)
+        //     def proc =  "git diff HEAD HEAD^ go.mod".execute()
+        //     proc.consumeProcessOutput(out, err)
+        //     proc.waitFor()
+        //     def stdOut = out.toString()
+        //     def errOut = err.toString()
 
-            if (stdOut) {
-                print("there are dependencies change")
-                buildBaseImage()
-                // new docker build and push to harbor
-            } else if (errOut) {
-                print("there are some errors to execute git command")
-                // go.mod, go.sum이 없는경우에도 standarderr가 나오고 이 스코프로 들어온다.
-                // 다시 빌드하는게 맞을까?
-                buildBaseImage()
-            } else {
-                print("there are no dependencies change")
-            }
-        }
+        //     if (stdOut) {
+        //         print("there are dependencies change")
+        //         buildBaseImage()
+        //         // new docker build and push to harbor
+        //     } else if (errOut) {
+        //         print("there are some errors to execute git command")
+        //         // go.mod, go.sum이 없는경우에도 standarderr가 나오고 이 스코프로 들어온다.
+        //         // 다시 빌드하는게 맞을까?
+        //         buildBaseImage()
+        //     } else {
+        //         print("there are no dependencies change")
+        //     }
+        // }
         stage('Build') {
             def dockerfile = 'dockerfile.build'
             def dockerImage = docker.build("jaeho-study-build:${env.BRANCH_NAME}", "-f ${dockerfile} .")
