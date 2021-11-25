8adb75b1bdaee6fd88670777cfc66d544c2dafe0
jaeho choi <chlwogh1993@gmail.com>
2021-11-25 15:08:16 +0900

    commit

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -48,7 +48,7 @@ node {
             def job_folder = "${env.WORKSPACE}/jenkins-sutdy"
             print('2222222222222222222222222222222222222222222222222222')
             def dockerfile = 'dockerfile.test'
-            def dockerimage = docker.image("jaeho-study-build:${env.BRANCH_NAME}", "-f ${dockerfile} .").inside {
+            docker.image("jaeho-study-build:${env.BRANCH_NAME}", "-f ${dockerfile} .").inside() {
                 sh './entrypoint-test.sh'
             }
             print('333333333333333333333333333333333333333333333333333')
